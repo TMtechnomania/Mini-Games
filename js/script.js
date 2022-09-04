350ed9d5c1b3f4004483de51dda0c4385a000a1b
TMtechnomania <tiwari.kartik3002@gmail.com>
2022-09-04 18:23:28 +0530

    animates tiles with 3 section and logo section

--- js/script.js
+++ js/script.js
@@ -3,18 +3,24 @@ carousel();
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("blur-bg");
-  var y = document.getElementsByClassName("blur-bg2");
+  var x = document.getElementsByClassName("slide");
+  var y = document.getElementsByClassName("slide2");
+  var z = document.getElementsByClassName("slide3");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    x[i].style.opacity = "0";
   }
   for (i = 0; i < y.length; i++) {
-    y[i].style.display = "none";
+    y[i].style.opacity = "0";
+  }
+  for (i = 0; i < z.length; i++) {
+    z[i].style.opacity = "0";
   }
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
+  x[slideIndex-1].style.opacity = "1";
   if (slideIndex > y.length) {slideIndex = 1}
-  y[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  y[slideIndex-1].style.opacity = "1";
+  if (slideIndex > z.length) {slideIndex = 1}
+  z[slideIndex-1].style.opacity = "1";
+  setTimeout(carousel, 2500); // Change image every 2 seconds
 }
